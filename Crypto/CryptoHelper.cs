7a253fb5b90c705cc4cff83e15d739b9b70602b5
Furqan Shaikh <furqan_shaikh_1999@hotmail.com>
2016-07-30 22:46:03 +0530

    First Commit

---
+++ Crypto/CryptoHelper.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CryptoHelper
+    {
+        #region Members
+        private static int SALTLENGTH = 8;
+        #endregion Members
+
+        #region Public Methods
+
+        #region Salt Methods
+
+        public static byte[] GenerateSalt(int saltLength = 32)
+        {
+            using (RNGCryptoServiceProvider randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var salt = new byte[saltLength];
+                randomNumberGenerator.GetBytes(salt);
+
+                return salt;
+            }
+        }
+
+        #endregion Salt Methods
+
+        #region Hashing Functions
+        // Hashing Algorithms:
+        // MD5 - DO NOT USE. Not Secure
+        // SHA-1 - DO NOT USE. Not Secure
+        // SHA-2 (SHA-256, SHA-512) - USE
+        // SHA-3 - Not available as part of .NET Framework
+
+            /// <summary>
+            /// Computes hash of the given input using SHA-256 hashing algorithm
+            /// </summary>
+            /// <param name="inputVal">Input value to be hashed</param>
+            /// <returns>Hash</returns>
+        public static byte[] ComputeHashSha256(byte[] inputVal)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(inputVal);
+            }
+        }
+        #endregion Hashing Functions
+
+        #region Password-Related Methods
+
+        public static byte[] GeneratePasswordHash(byte[] userSalt, string password)
+        {
+            // 1. Convert password to bytes array
+            byte[] passwordBytes = CryptoHelper.ToBytes(password);
+            // 2. Combine the password and salt to
+            byte[] saltedPassword = CryptoHelper.Combine(passwordBytes, userSalt);
+            // 3. Compute hash of the combined value
+            byte[] hashedPassword = CryptoHelper.ComputeHashSha256(saltedPassword);
+            return hashedPassword;
+        }
+
+        public static byte[] GeneratePasswordHash(byte[] salt,string password,int noOfIterations)
+        {
+            // 1. Convert password to bytes array
+            byte[] passwordBytes = CryptoHelper.ToBytes(password);
+
+            using (Rfc2898DeriveBytes rfc2898 = new Rfc2898DeriveBytes(passwordBytes, salt, noOfIterations))
+            {
+                return rfc2898.GetBytes(32);
+            }
+        }
+        #endregion Password-Related Methods
+
+        #region Helper Functions
+
+        public static string ToBase64String(byte[] bytes)
+        {
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static byte[] ToBytes(string stringVal)
+        {
+            return Encoding.UTF8.GetBytes(stringVal);
+        }
+
+        #endregion Helper Functions
+
+        #endregion Public Methods
+
+        #region Private Methods
+        private static byte[] Combine(byte[] first,byte[] second)
+        {
+            var outputByteArray = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, outputByteArray, 0, first.Length);
+            Buffer.BlockCopy(second, 0, outputByteArray, first.Length, second.Length);
+
+            return outputByteArray;
+        }
+        #endregion Private Methods
+    }
+}
---
+++ Crypto/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Salt " + i + " : " + CryptoHelper.ToBase64String(CryptoHelper.GenerateSalt(32)));
+            //}
+
+            const string Message = "P@ssw0rd1988";
+
+            // 1. Get the user salt
+            byte[] salt = CryptoHelper.GenerateSalt();
+
+
+            // 2. Generate password hash
+            byte[] hashedPassword = CryptoHelper.GeneratePasswordHash(salt, Message);
+            string passwordHashString = CryptoHelper.ToBase64String(hashedPassword);
+            //Console.WriteLine("Salt: " + CryptoHelper.ToBase64String(salt));
+            //Console.WriteLine("Password hash: " + passwordHashString);
+
+            // PBKDF2
+            Pbkdf(salt, Message, 100);
+            Pbkdf(salt, Message, 1000);
+            Pbkdf(salt, Message, 5000);
+            Pbkdf(salt, Message, 10000);
+            Pbkdf(salt, Message, 50000);
+            Pbkdf(salt, Message, 100000);
+            Pbkdf(salt, Message, 200000);
+            Pbkdf(salt, Message, 500000);
+        }
+
+        private static void Pbkdf(byte[] salt,string password,int iterations)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var pbkdf1 = CryptoHelper.ToBase64String(CryptoHelper.GeneratePasswordHash(salt, password, iterations));
+
+            sw.Stop();
+            
+            Console.WriteLine(string.Format("Iterations: {0} Hash: {1} Time: {2}ms", iterations, pbkdf1, sw.ElapsedMilliseconds));
+        }
+    }
+}
